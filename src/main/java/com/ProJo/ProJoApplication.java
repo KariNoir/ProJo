7b35044d980f29de8fc0d09cff8725f0108025e1
mcheich <mcheich@gmail.com>
2020-03-25 14:44:53 -0400

    JPA

---
+++ src/main/java/com/ProJo/ProJoApplication.java
@@ -0,0 +1,13 @@
+package com.ProJo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProJoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProJoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProJo/ProJoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProJo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProJoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
